b9ce976e1c663785b7607b51e5fbb6e108202e9e
ecollazodominguez <ecollazodominguez@nautilus9.danielcastelao.org>
2019-02-28 09:37:48 +0100

    ejercicio

--- src/Main/Colecciones.java
+++ src/Main/Colecciones.java
@@ -3,11 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package hashmap;
+package Main;
 
 import Adapter.Hash2List;
 import ArrayList.ClassList;
 import ArrayList.MostrarClassList;
+import hashmap.ClassHash;
 
 /**
  *
